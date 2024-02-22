6e8158a38866c8e07649d23b2db2099b1aa7b7cb
JClark-0 <clarj524@newschool.edu>
2024-02-22 13:49:56 -0500

    files for assignment 4

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+
+// --------------  Class 4 Assignment: Homegrown "AI" Chatbot --------------  
+
+/* Consider that in their simplest form, all AI assistants are Question & Answer database querying program. You make a list of questions and their possible answers and when a question is asked, the bot will check against the database and find the matching answer. More complex ones will learn, weigh options, add to the questions list, search for patterns and eventually be able to make connections between answers using fuzzy logic and neural nets and generate more subtle interpretations.
+
+Though you can try to build that yourself in a future assignment, for the purposes of this exercise, let's keep this simple and in context of testing your Object building knowledge.
+
+1. Create a list of questions and their corresponding answers within the object as a property (this can be as an array or questions and answers or a combination of an array or object or something similar).
+
+2. Then create a method (or methods) that checks a question (or word or phrase) you provide as an argument to it and then performs a search/lookup to find the closest matching question to the one asked and return its answer. Think of it like your own text-based Messenger helper/Slackbot/ChatGPT AI Assistant etc.
+
+3. Keep it simple and try not to get too embroiled in the logic, but yes, there will be logic issues you will need to deal with, so start slow and simple and build upon it.
+
+4. Start by building an object that contains all the properties of the AI in it. For illustration, here's some code that uses only arrays and functions to do this. But your solution needs to use the object format (as you did in class with the dice Object):
+*/
+
+
+
+console.log ('Class 4 Assignment: Homegrown "AI" Chatbot\n-------------------------------------------');
